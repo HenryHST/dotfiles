9e4bf15eae5031006ab107282959c0374dcd96b8
Henry Stadthagen <h.stadthagen@me.com>
2024-09-28 17:05:32 +0200

    add new files

---
+++ .zsh/aliases.zsh
@@ -0,0 +1,18 @@
+# Alias
+# ---
+#
+alias k="kubectl"
+alias h="helm"
+alias tf="terraform"
+alias a="ansible"
+alias ap="ansible-playbook"
+
+alias code="open -a 'Visual Studio Code'"
+
+alias gprj="cd $HOME/Projects"
+alias gcs="cd $HOME/Projects/henry/cheat-sheets"
+alias gbp="cd $HOME/Projects/henry/boilerplates"
+
+alias ls="eza --icons --group-directories-first"
+alias ll="eza --icons --group-directories-first -l"
+#alias grep='grep --color'
